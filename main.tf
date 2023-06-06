afad533d6bc0fb76fd651cf46f80ac3890a23078
Garima Agrawal <garima.agrawal@vsodir.onmicrosoft.com>
2023-06-06 12:02:42 +0200

    Allowed public access to blob

--- main.tf
+++ main.tf
@@ -16,6 +16,8 @@ resource "azurerm_storage_account" "stg_acc" {
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
   min_tls_version          = "TLS1_0"
+  enable_https_traffic_only = true
+  allow_nested_items_to_be_public = true
 
   static_website {
     index_document = "index.html"
